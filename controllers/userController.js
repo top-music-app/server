ff0fc690e8646e7d8db4d65b3ebe856ef3744b38
pputra <mynameisyoga@live.com>
2018-10-05 02:11:18 +0700

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const axios = require('axios');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const Crypto = require('../helpers/crypto');
 
 
 module.exports = {
@@ -71,5 +72,54 @@ module.exports = {
         }).catch((err) => {
             res.status(500).json(err);
         });
-            }
+    },
+
+    register: (req, res) => {
+
+        req.body.password = Crypto.encrypt(req.body.password);
+        
+        User.create({
+            email: req.body.email,
+            password: req.body.password,
+            oauth: 0,
+            name: req.body.name
+        }).then((user) => {
+
+            let jwttoken = jwt.sign({
+                email: user.email,
+                id: user._id
+            }, process.env.JWT_KEY);
+
+            res.status(200).json({
+                jwttoken: jwttoken
+            });
+
+        }).catch((err) => {
+            res.status(500).json({
+                message: 'server error'
+            })
+        });
+    },
+
+    login: (req, res) => {
+        console.log(req.body);
+        req.body.password = Crypto.encrypt(req.body.password);
+
+        User.findOne({
+            
+            email: req.body.email,
+            password: req.body.password
+            
+        }).then((user) => {
+            console.log('user has been found');
+            console.log(user);
+            let jwttoken = jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY)
+            console.log(jwttoken);
+            res.status(200).json({jwttoken: jwttoken});
+        }).catch((err) => {
+            res.status(500).json({
+                message: 'user is not found'
+            });
+        });
+    }
 };
