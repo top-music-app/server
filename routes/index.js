84c73daa306b289b6677ee4211274b368f9dadcb
pputra <mynameisyoga@live.com>
2018-10-04 20:02:41 +0700

    finish sending token for google login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('login');
 });
+
+router.post('/signin/google', UserController.loginGoogle);
 
 module.exports = router;
