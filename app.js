fd506d0fcd5045b95f92d7d6b9f42181e9dfd97a
pputra <mynameisyoga@live.com>
2018-10-04 19:02:53 +0700

    setup db

--- app.js
+++ app.js
@@ -4,18 +4,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+<<<<<<< HEAD
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/top-music-app', {useNewUrlParser: true});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var favoritesRouter = require('./routes/favorites')
+=======
+const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const cors = require('cors');
+>>>>>>> setup db
 
 var app = express();
+app.use(cors());
+mongoose.connect('mongodb://localhost/topmusicapp');
+
+
+<<<<<<< HEAD
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+=======
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+>>>>>>> setup db
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
